e4b02db3239595b0eaef0063d26d7d85ba0f988b
kafka <kafka@linux1.texc3uirjkgupnk1bswumgk2ac.ax.internal.cloudapp.net>
2022-01-18 20:55:26 +0000

    added fiels for testing GE kafka patch

---
+++ ge_consumer.py
@@ -0,0 +1,30 @@
+from json import loads
+import io
+import kafka
+from kafka import KafkaConsumer
+import avro
+#import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+
+file = "scadastatus.avsc"
+schema = avro.schema.parse(open(file ,"rb").read())
+reader = avro.io.DatumReader(schema)
+
+consumer = KafkaConsumer(
+    'vku-tst',
+     bootstrap_servers=['10.0.0.4:9092'],
+     group_id = 'vijay08',
+     auto_offset_reset='earliest')
+
+
+#bytes_data =  bytes_reader.getvalue()
+print(consumer)
+for message in consumer:
+    message = message.value
+#    message = message.value
+#    print(message)
+    bytes_reader = io.BytesIO(message)
+    decoder = avro.io.BinaryDecoder(bytes_reader)
+    print(reader.read(decoder))
+    consumer.commit()
---
+++ ge_produce.py
@@ -0,0 +1,35 @@
+from time import sleep
+from json import dumps, encoder
+from json import loads
+import io
+import kafka
+import avro
+#import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+
+#logging.basicConfig(level=logging.DEBUG)
+
+file = "scadastatus.avsc"
+schema = avro.schema.parse(open(file ,"rb").read())
+writer = avro.io.DatumWriter(schema)
+reader = avro.io.DatumReader(schema)
+bytes_writer = io.BytesIO()
+encoder = avro.io.BinaryEncoder(bytes_writer)
+
+producer = kafka.KafkaProducer(bootstrap_servers=['10.0.0.4:9092']
+                         )
+
+writer.write({"id": "15986c5f-e164-4b3d-8a7a-e959feda71c5", "name": "AVC.DK1.IDU.AVC4", "value": 10, "quality1":10, "quality2": 20, "time": 10},encoder)
+raw_bytes = bytes_writer.getvalue()
+
+print(raw_bytes)
+try:
+ producer.send('vku-tst',raw_bytes)
+ producer.flush()
+except:
+ print("error")   
+
+bytes_reader = io.BytesIO(raw_bytes)
+decoder = avro.io.BinaryDecoder(bytes_reader)  
+print(reader.read(decoder))
